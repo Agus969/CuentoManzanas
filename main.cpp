400d9263d97b17026f5dd8281e07ae45f4485f7c
Agus969 <agus.tin.2024@outlook.com>
2024-07-27 19:07:52 +0200

    este es el segundo programa que subo de prueba

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 10;
+  do {
+    cout << i << " Manzanitas tiene el manzano, y de:" << i <<",una cayo \n"; 
+    i--;
+  }
+  while (i > 0);
+  cout<<"este cuento se acabo";
+  return 0;
+}
